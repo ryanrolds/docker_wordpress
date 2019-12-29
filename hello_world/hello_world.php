7ffdc99b390d04d6dcff8f68dfefbfa6f5a8b989
Ryan Olds <ryanrolds@gmail.com>
2019-12-28 17:43:19 -0800

    Added start of a practice theme and more

--- hello_world/hello_world.php
+++ hello_world/hello_world.php
@@ -13,6 +13,6 @@ License: MIT
 add_action( 'the_content', 'my_thank_you_text' );
 
 function my_thank_you_text ( $content ) {
-    return $content .= '<p>Thank you for reading!</p>';
+    return $content .= '<p>Thank you for reading!!!!</p>';
 }
 ?>
